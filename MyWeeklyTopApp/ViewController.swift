b70cd926a44b757446523aa62d6bea709a10df19
hYdrago1 <vanyagoncha@gmail.com>
2025-10-26 13:20:19 +0100

    Initial Commit

---
+++ MyWeeklyTopApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeeklyTopApp
+//
+//  Created by Ivan Honcharov on 26.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
